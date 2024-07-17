f5fa4e4d78005489708c4ad8c1da6962f0992c16
Max O'Dell <maxodell11@gmail.com>
2024-07-17 19:11:41 +1000

    Implement API Gateway and connect to Lambda function

--- deployment/main.tf
+++ deployment/main.tf
@@ -30,12 +30,20 @@ resource "aws_iam_role" "lambda_iam_role" {
 }
 
 // Zip the lambda function's entire directory.
-// TODO: Move to using S3 Bucket instead of zip archive approach. 
+// TODO: Move to using S3 Bucket instead of zip archive approach.
 data "archive_file" "lambda-main" {
   type = "zip"
 
   source_dir  = "${path.module}/../src/lambda/main"
   output_path = "${path.module}/../bin/lambda-main.zip"
+}
+
+// Load in Discord API Public Key using ASM.
+data "aws_secretsmanager_secret" "discord-api-public-key" {
+  arn = "arn:aws:secretsmanager:ap-southeast-2:211125624925:secret:kao-the-third/discord-public-key-8HhrB5"
+}
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.discord-api-public-key.id
 }
 
 // Create Lambda function from zip archive.
@@ -48,11 +56,66 @@ resource "aws_lambda_function" "main" {
 
   runtime = "nodejs20.x"
   handler = "index.handler"
+
+  environment {
+    variables = {
+      PUBLIC_KEY = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["PUBLIC_KEY"]
+    }
+  }
 }
 
-// Create a CloudWatch log group.
-resource "aws_cloudwatch_log_group" "main" {
-  name = "/aws/lambda/${aws_lambda_function.main.function_name}"
+// Create REST API.
+resource "aws_api_gateway_rest_api" "main" {
+  name = "kao-the-third"
+}
 
-  retention_in_days = 7
+// Create resource (route) - greedy proxy route for Discord API.
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "{discord+}"
+}
+
+// Create proxy method.
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+// Create Lambda integration, pointing it to both the proxy method and the Lambda function.
+resource "aws_api_gateway_integration" "main-lambda-integration" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.main.invoke_arn
+}
+
+// Create deployment resource.
+resource "aws_api_gateway_deployment" "api-deployment" {
+  depends_on = [
+    aws_api_gateway_integration.main-lambda-integration
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  stage_name  = "dev"
+}
+
+// Grant the API Permission to interact with the Lambda function.
+resource "aws_lambda_permission" "api-to-lambda-permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
+}
+
+// Output the API URL after creation.
+output "api_url" {
+  value = aws_api_gateway_deployment.api-deployment.invoke_url
 }
